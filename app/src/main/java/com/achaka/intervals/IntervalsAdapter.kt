b036523cd1a3fd810bf5e0aa956ea73759fd7442
Achaka <oen-feedback@yandex.ru>
2021-07-12 00:36:17 +0300

    adapter work

---
+++ app/src/main/java/com/achaka/intervals/IntervalsAdapter.kt
@@ -0,0 +1,36 @@
+package com.achaka.intervals
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.achaka.intervals.databinding.IntervalItemBinding
+
+class IntervalsAdapter : ListAdapter<Interval, IntervalsAdapter.IntervalViewHolder>(DiffCallback) {
+
+    private val intervals: List<Interval> = mutableListOf()
+
+    fun setData(list: List<Interval>) {
+
+    }
+
+    class IntervalViewHolder(private var binding: IntervalItemBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(interval: Interval) {
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IntervalViewHolder {
+        val viewHolder = IntervalViewHolder(
+            IntervalItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+
+            )
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: IntervalViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/achaka/intervals/TrainingsAdapter.kt
@@ -0,0 +1,4 @@
+package com.achaka.intervals
+
+class TrainingsAdapter {
+}
