4a44af377ab47a8620023d9dcf83f360e0607555
Achaka-w <oen-feedback@gmail.com>
2021-07-13 13:23:31 +0300

    pace converter done

--- app/src/main/java/com/achaka/intervals/paceconverter/PaceConverter.kt
+++ app/src/main/java/com/achaka/intervals/paceconverter/PaceConverter.kt
@@ -2,7 +2,23 @@ package com.achaka.intervals.paceconverter
 
 class PaceConverter {
 
-    fun convertMilePaceToKmPace(milePace: String) {
-        milePace
+    fun convertMilePaceToKmPace(secondsOnMile: Int): Float {
+        return secondsOnMile/1.60934F
+    }
+
+    fun convertKmPaceToMilePace(secondsOnKm: Int): Float {
+        return secondsOnKm*0.62137F
+    }
+
+    fun minutesStringToSeconds(timeString: String): Int {
+        val minutes = Integer.parseInt(timeString.substringBefore(":"))
+        val seconds = Integer.parseInt(timeString.substringAfter(":"))
+        return minutes*60+seconds
+    }
+
+    fun secondsToMinutesString(seconds: Int): String {
+        val minutes = seconds/60
+        val remainingSeconds = seconds%60
+        return "$minutes:$remainingSeconds"
     }
 }
