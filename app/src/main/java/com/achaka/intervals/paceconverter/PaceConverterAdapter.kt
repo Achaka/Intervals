5fa1b386995e0ec88d5b892068cbb28fb596f670
Achaka-w <oen-feedback@gmail.com>
2021-07-21 18:30:43 +0300

    pace history adapter

--- app/src/main/java/com/achaka/intervals/paceconverter/PaceConverterAdapter.kt
+++ app/src/main/java/com/achaka/intervals/paceconverter/PaceConverterAdapter.kt
@@ -1,4 +1,35 @@
 package com.achaka.intervals.paceconverter
 
-class PaceConverterAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.achaka.intervals.databinding.PaceHistoryItemBinding
+
+class PaceConverterAdapter: RecyclerView.Adapter<PaceConverterAdapter.PaceConverterViewHolder>() {
+
+    var history = mutableListOf<Pace>()
+
+    fun setData(list: MutableList<Pace>) {
+        this.history = list
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PaceConverterViewHolder {
+        val viewHolder = PaceConverterViewHolder(PaceHistoryItemBinding.inflate(
+                LayoutInflater.from(parent.context)
+        ))
+
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: PaceConverterViewHolder, position: Int) {
+        holder.bind(history[position])
+    }
+
+    override fun getItemCount(): Int = history.size
+
+    class PaceConverterViewHolder(private val binding: PaceHistoryItemBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(pace: Pace) {
+            binding
+        }
+    }
 }
