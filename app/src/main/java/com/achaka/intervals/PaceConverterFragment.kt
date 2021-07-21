4afa608d521fe3bcfbd8725a943f049a12558551
Achaka-w <oen-feedback@gmail.com>
2021-07-21 16:44:46 +0300

    pace layout

---
+++ app/src/main/java/com/achaka/intervals/PaceConverterFragment.kt
@@ -0,0 +1,20 @@
+package com.achaka.intervals
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PaceConverterFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pace_converter, container, false)
+    }
+}
