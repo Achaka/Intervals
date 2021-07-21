29728068bd04c3ea11cfdc9dc0d82030ed07196a
Achaka-w <oen-feedback@gmail.com>
2021-07-21 14:41:13 +0300

    navigation drawer attempt

--- app/src/main/java/com/achaka/intervals/MainActivity.kt
+++ app/src/main/java/com/achaka/intervals/MainActivity.kt
@@ -2,10 +2,26 @@ package com.achaka.intervals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        findViewById<NavigationView>(R.id.nav_view)
+                .setupWithNavController(navController)
     }
 }
